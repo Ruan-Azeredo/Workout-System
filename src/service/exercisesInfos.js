b2d22becaeb7b873d5e971bc7236a7994ddb2040
Ruan-M2 <ruanazeredo@gmail.com>
2023-11-01 15:32:40 -0300

    :construction: build: add more exercises

--- src/service/exercisesInfos.js
+++ src/service/exercisesInfos.js
@@ -34,5 +34,41 @@ export const infos = [
         name: 'Panturrilha um pé só',
         description: 'A posição inicial é em pé, sobre a planta de um dos pés: enquanto um pé fica reto no chão, o joelho oposto é flexionado e o pé correspondente ao joelho flexionado fica na vertical, apoiando-se pela ponta na parte de trás pé plantado no chão. faça 10 de um pé depois 10 do outro apra completar uma serie. Utilize uma cadeira de apoio',
         img: 'https://www.mundoboaforma.com.br/wp-content/uploads/2021/01/37121301-Standing-Single-Leg-Calf-Raise-with-Support-female_Calves_360-logo.gif'
+    },
+    {
+        id: 6,
+        name: 'Escalador',
+        description: 'No escalador, a pessoa deve apoiar as duas mãos no chão e realizar um movimento de alternar a posição das pernas, como se estivesse subindo uma montanha. Levantar uma perna depois a outra para contar uma repetição',
+        img: 'https://www.smartfit.com.br/news/wp-content/uploads/2020/09/benef%C3%ADcios-do-HIIT-escalador.jpg'
+    },
+    {
+        id: 7,
+        name: 'Abdominal Bicicleta',
+        description: 'Aqui, as pernas devem ficar elevadas o tempo todo, realizando um movimento de pedalada, com uma perna sendo esticada à frente e a outra flexionada alternadamente. levantar uma perna, apos a outra qualifica uma repetição',
+        img: 'https://fitpeople.com/pt/wp-content/uploads/2018/07/abdominais-zona-media-exercicio.jpg'
+    },
+    {
+        id: 8,
+        name: 'Ponte Quadril',
+        description: 'Deite-se de costas, com as pernas flexionadas e os braços estendidos ao lado do tronco. Contraia os glúteos e o abdome e eleve o quadril, formando uma linha reta dos joelhos aos ombros.',
+        img: 'https://vitat.com.br/wp-content/uploads/2019/11/thinkstockphotos-620737596-e1577383677406.jpg?ims=fit-in/900x350/filters:quality(70):format(webpp)'
+    },
+    {
+        id: 9,
+        name: 'Prancha',
+        description: 'Neste exercicio, cada repetição equivale a 5 segundos, ou seja, caso voce tenha que fazer 3 series de 10 repetições, são 3 series de 50 segundos de prancha.',
+        img: 'https://i0.statig.com.br/bancodeimagens/dy/07/wh/dy07whmc9civh7r092jaizsx6.jpg'
+    },
+    {
+        id: 10,
+        name: 'Rotação Russa',
+        description: 'Deite-se de barriga para cima, com o tronco levemente inclinado e as pernas semiflexionadas e erguidas. Contraia o abdome e gire o tronco para a esquerda e, depois, para direita, em um movimento controlado. Isso é uma repetição.',
+        img: 'https://media.giphy.com/media/cpKD9u3S25xYL8tcbr/giphy.gif'
+    },
+    {
+        id: 11,
+        name: 'Abdominal Supra',
+        description: 'Apoie as duas mãos na cabeça, mantenha as pernas abertas e com as respectivas solas dos pés em contato com solo. Faça a elevação dos ombros do chão, fazendo a contração do abdômen; evitem puxar a cabeça; a mão na cabeça deve servir somente de apoio para a mesma.',
+        img: 'https://s2-ge.glbimg.com/P6A7cYQImMWwWvZdsrwKgRHz1n0=/0x0:1600x1060/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2018/T/l/b6BR5oQ1aPKkkG5TLCBQ/whatsapp-image-2018-09-11-at-19.29.48.jpeg'
     }
 ]
